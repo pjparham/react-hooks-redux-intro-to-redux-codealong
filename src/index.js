76cd3139cc5db3f4efc1708630f6aa0983d69f4c
Parker Parham <parkerparham@gmail.com>
2023-02-14 17:12:25 -0500

    Adds redux to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
+
+const store = createStore(counterReducer);
 
 ReactDOM.render(<App />, document.getElementById("root"));
