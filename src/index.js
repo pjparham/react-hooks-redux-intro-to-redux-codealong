81bbd855a6ee8b7b8c3fe5083d8883651d47e1a2
Parker Parham <parkerparham@gmail.com>
2023-02-15 12:11:45 -0500

    adds Redux devtools access and adds console logs for testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
-const store = createStore(counterReducer);
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ); /* code change */
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById("root"));
